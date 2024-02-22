cd66908e3f2e130954b8bd33cce69ea416fa1cb4
04tmoody <travismoody04@gmail.com>
2024-02-21 18:53:21 -0700

    Fix weird canvas display issues

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 c = document.getElementById("canvas");
 ctx = c.getContext("2d");
 
-const WIDTH = 30;
+const WIDTH = 25;
+
+function resize() {
+    c.width = Math.min(window.innerWidth, window.innerHeight)*0.6;
+    c.height = Math.min(window.innerWidth, window.innerHeight)*0.6;
+}
 
 function render() {
+    grid = document.getElementById("grid").checked;
+
     ctx.fillStyle = "white";
     ctx.fillRect(0,0,c.width,c.height);
 
@@ -11,12 +18,13 @@ function render() {
     const sizey = c.height/WIDTH;
     for (let x=0; x<c.width; x+=sizex) {
         for (let y=0; y<c.height; y+=sizey) {
-            ctx.strokeRect(x,y,sizex,sizey);
+            if (grid) ctx.strokeRect(x,y,sizex,sizey);
         }
     }
 }
 
 function main() {
+    resize();
     render();
     requestAnimationFrame(main);
 }
