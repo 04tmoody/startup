10366c0c30f0f127e884032663137ef0a719c389
04tmoody <travismoody04@gmail.com>
2024-03-06 10:00:52 -0700

    Serving up from public directory

--- server.js
+++ server.js
@@ -1,2 +1,9 @@
 const express = require('express');
 const app = express();
+
+app.use(express.static('public'));
+
+const port = process.argv.length > 2 ? process.argv[2] : 3000;
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
