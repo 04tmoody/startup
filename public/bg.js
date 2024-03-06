d01fb29b290d83c0a9c4e587a9dbf2a496cb5e3b
04tmoody <travismoody04@gmail.com>
2024-03-06 14:46:39 -0700

    Made background loading smoother

--- public/bg.js
+++ public/bg.js
@@ -1,9 +1,16 @@
 function displayBackground(data) {
-    fetch('https://php-noise.com/noise.php?hex=${hex}&json')
-      .then((response) => response.json())
-      .then((data) => {
+  fetch('https://php-noise.com/noise.php?hex=${hex}&json')
+    .then((response) => response.json())
+    .then((data) => {
+      const img = new Image();
+      img.onload = () => {
         document.body.style.backgroundImage = `url('${data.uri}')`;
+      };
+      img.onerror = () => {
+        console.error("Error loading image:", data.uri);
+      };
+      img.src = data.uri;
     });
 }
-  
+
 displayBackground();
