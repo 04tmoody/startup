384446e9191a1d3f1a454f90820533fb39b9afe6
04tmoody <travismoody04@gmail.com>
2024-03-06 10:01:50 -0700

    Default logic

--- server.js
+++ server.js
@@ -7,3 +7,8 @@ const port = process.argv.length > 2 ? process.argv[2] : 3000;
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
 });
+
+// Return the application's default page if the path is unknown
+app.use((_req, res) => {
+    res.sendFile('index.html', { root: 'public' });
+});
