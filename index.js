1b6c181db41079316df1c43171244f532d00c2e2
04tmoody <travismoody04@gmail.com>
2024-03-20 10:50:14 -0600

    Login functionality on Server

--- index.js
+++ index.js
@@ -4,13 +4,51 @@ const bcrypt = require('bcrypt');
 const express = require('express');
 const app = express();
 
+const authCookieName = 'token';
+
 // Import Database Code
 const DB = require('./database.js');
 
+// JSON body parsing using built-in middleware
 app.use(express.json());
+
+// Use the cookie parser middleware for tracking authentication tokens
+app.use(cookieParser());
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
+
+// Trust headers that are forwarded from the proxy so we can determine IP addresses
+app.set('trust proxy', true);
+
+// CreateAuth token for a new user
+apiRouter.post('/auth/create', async (req, res) => {
+    if (await DB.getUser(req.body.name)) {
+      res.status(409).send({ msg: 'Existing user' });
+    } else {
+      const user = await DB.createUser(req.body.name, req.body.password);
+  
+      // Set the cookie
+      setAuthCookie(res, user.token);
+  
+      res.send({
+        id: user._id,
+      });
+    }
+  });
+
+  // GetAuth token for the provided credentials
+apiRouter.post('/auth/login', async (req, res) => {
+    const user = await DB.getUser(req.body.name);
+    if (user) {
+      if (await bcrypt.compare(req.body.password, user.password)) {
+        setAuthCookie(res, user.token);
+        res.send({ id: user._id });
+        return;
+      }
+    }
+    res.status(401).send({ msg: 'Unauthorized' });
+  });
 
 // loadBoard creates a new empty board
 function loadBoard(size) {
