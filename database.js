17b6a5ee2321bde2465e338cbc573a5272a01f5a
04tmoody <travismoody04@gmail.com>
2024-03-20 09:20:43 -0600

    Add board functionality to Database

--- database.js
+++ database.js
@@ -6,6 +6,7 @@ const config = require('./dbConfig.json');
 const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
 const client = new MongoClient(url);
 const db = client.db('rental');
+const boardCollection = db.collection('board');
 
 (async function testConnection() {
   await client.connect();
@@ -14,3 +15,13 @@ const db = client.db('rental');
   console.log(`Unable to connect to database with ${url} because ${ex.message}`);
   process.exit(1);
 });
+
+function getSquare(x,y) {
+  return boardCollection.findOne({ x:x, y:y });
+}
+
+async function setSquare(x, y, color) {
+  const filter = { x:x, y:y };
+  const update = { $set: { color: color } };
+  await scoreCollection.updateOne(filter, update);
+}
