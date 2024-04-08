67eb4d957c2d652c311685b5b271c8ec8f9be4e4
04tmoody <travismoody04@gmail.com>
2024-04-08 17:48:31 -0600

    Set up main page

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import './log.js';
 
 export function Main() {
   return (
-    <main className='container-fluid bg-secondary text-center'>
-      <div>Main page displayed here</div>
+    <main className="container">
+      <div id="sidebar">
+        <span id="yourname">{localStorage.getItem('username') ? localStorage.getItem('username') : "Log In to Draw"}</span>
+        <div id="message-log">
+        <ul style={{listStyleType: 'none', padding: 0}} id="messages">
+            <li>Message Log<br/>-----------</li>
+          </ul>
+        </div>
+      </div>
+      <div id="canvas-container">
+        <canvas id="canvas" width="400px" height="400px"></canvas>
+      </div>
     </main>
   );
 }
