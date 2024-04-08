7a3bfc33f704d0e3e63a56598c2eda506518406b
04tmoody <travismoody04@gmail.com>
2024-04-08 11:41:34 -0600

    About page

--- src/about.jsx
+++ src/about.jsx
@@ -2,8 +2,13 @@ import React from 'react';
 
 export function About() {
   return (
-    <main className='container-fluid bg-secondary text-center'>
-      <div>About page displayed here</div>
+    <main className="about-main">
+      <div className="about">
+        <img src="https://avatars.githubusercontent.com/u/49120372"/>
+        <p>
+        Retro Renditions is a website created by Travis Moody, a student at Brigham Young University for his CS260 class. Hope you enjoy it!
+        </p>
+      </div>
     </main>
   );
 }
