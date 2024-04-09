acf125f9c309b4101362ae7c5995c6f7ebb698f6
04tmoody <travismoody04@gmail.com>
2024-04-08 21:51:00 -0600

    Set up join page

--- src/join.jsx
+++ src/join.jsx
@@ -2,8 +2,16 @@ import React from 'react';
 
 export function Join() {
   return (
-    <main className='container-fluid bg-secondary text-center'>
-      <div>Join page displayed here</div>
+    <main className="join-main">
+        <form className="join" action="index.html" method="get" onsubmit="create(event)">
+            <h1 style={{color: "black", fontFamily: 'Press Start 2P', fontSize: "20px"}}>Create<br/>Account</h1><br/><br/>
+            <label htmlFor="username2">Username:</label>
+            <input type="text" id="username2" placeholder="Username"  name="username2" required/><br/>
+            <label htmlFor="password2">Password:</label>
+            <input type="password" id="password2" placeholder="Password" name="password2" required/><br/>
+            <p id="error2" style={{padding: 0, margin: 0, fontSize: "11px", color:"red", display:"none"}}>Username Taken</p>
+            <input type="submit" className="submit" value="Create Account"/>
+        </form>
     </main>
   );
 }
